df98c2c011660ef4761e26c28f992f6746a28a01
rafaelsteil <rafaelsteil>
2006-02-13 13:39:07 +0000

    *** empty log message ***

--- WEB-INF/config/database/mysql/mysql.sql
+++ WEB-INF/config/database/mysql/mysql.sql
@@ -1,4 +1,1 @@
-#Tue Feb 07 16:19:06 BRST 2006
-PermissionControl.getRoleIdsByGroup=SELECT DISTINCT rv.role_id FROM jforum_role_values rv, jforum_roles r WHERE r.role_id \= rv.role_id AND r.group_id \= ?
-PermissionControl.deleteAllRoleValues=DELETE jforum_role_values FROM jforum_role_values rv, jforum_roles r WHERE r.role_id \= rv.role_id AND r.group_id \= ?
-PermissionControl.deleteRoleValuesByRoleId=DELETE FROM jforum_role_values WHERE role_id IN (?)
+#Sun Feb 12 21:35:02 BRST 2006
