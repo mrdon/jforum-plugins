73c011e26483bc8337e44fb62e98db5b533849ff
rafaelsteil <rafaelsteil>
2006-10-18 02:02:23 +0000

    *** empty log message ***

--- www/website/new/header.js
+++
@@ -1,9 +0,0 @@
-// header.js
-function SymError(){return true;}
-function SymWinOpen(url, name, attributes){return (new Object());}
-function liO(idN){document.getElementsByTagName('LI')[idN].style.background = "url(img/tabs.gif) no-repeat 100% -400px";}
-function liU(idN){document.getElementsByTagName('LI')[idN].style.background = "url(img/tabs.gif) no-repeat 100% -600px";}
-function nav(idN){d=document.getElementsByTagName('DIV')[idN]; if (d.style.display=="block") { d.style.display="none"; } else { d.style.display="block"; }}
-var SymRealWinOpen = window.open;
-window.open = SymWinOpen;
-window.onerror = SymError;
