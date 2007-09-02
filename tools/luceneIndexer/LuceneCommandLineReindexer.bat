f55d060379a66b6ece7736a8a71ff84a3a947fff
rafaelsteil <rafaelsteil>
2007-09-02 01:38:35 +0000

    *** empty log message ***

--- tools/luceneIndexer/LuceneCommandLineReindexer.bat
+++ tools/luceneIndexer/LuceneCommandLineReindexer.bat
@@ -1,3 +1,3 @@
 @echo off
 set jf=..\..\
-java -Djava.ext.dirs=%jf%\lib;%jf%\web-inf\lib; -cp ..\bin net.jforum.tools.search.LuceneCommandLineReindexer %*
+java -Djava.ext.dirs=%jf%\lib;%jf%\web-inf\lib; -cp ..\bin;%jf%\WEB-INF\classes net.jforum.tools.search.LuceneCommandLineReindexer %*
--- tools/luceneIndexer/LuceneCommandLineReindexer.sh
+++ tools/luceneIndexer/LuceneCommandLineReindexer.sh
@@ -1,2 +1,2 @@
 jf=../../
-java -Djava.ext.dirs=$jf/lib:$jf/web-inf/lib -cp ../bin net.jforum.tools.search.LuceneCommandLineReindexer $@
+java -Djava.ext.dirs=$jf/lib:$jf/web-inf/lib -cp ../bin:$jf/WEB-INF/classes net.jforum.tools.search.LuceneCommandLineReindexer $@
